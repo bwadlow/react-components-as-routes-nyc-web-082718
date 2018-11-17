2edbd9b1d3b72175a76d86b8c74ec77f50f94f92
bwadlow <bwadlow@gmail.com>
2018-11-17 15:48:21 -0500

    Done.

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1 align="center">THIS IS THE APP HEADER</h1>
+      </div>
+    )
+  }
+}
+
+export default Home
